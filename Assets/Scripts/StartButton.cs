915190915564decff7a7fb7108a49b744940e97c
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-08-19 21:22:13 +0200

    start works

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,9 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class StartButton : MonoBehaviour
     {
-        
+        private void OnMouseDown()
+        {
+            BlockResolver.instance.ResolveRound();
+        }
+
+        private void OnMouseOver()
+        {
+            
+        }
     }
 }
