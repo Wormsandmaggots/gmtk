c1686de454ad1c23f421d6efe81c071d2dbc4885
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-08-20 13:41:26 +0200

    .

--- Assets/Scripts/ResetButton.cs
+++ Assets/Scripts/ResetButton.cs
@@ -1,7 +1,24 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
 namespace DefaultNamespace
 {
-    public class ResetButton
+    public class ResetButton : MonoBehaviour
     {
-        
+        private bool isClicking = false;
+        private void OnMouseDown()
+        {
+            if(isClicking) return;
+
+            isClicking = true;
+            
+            transform.DOLocalMove(Vector3.down * 0.1f, 0.2f).onComplete = () =>
+            {
+                BlockResolver.instance.ResolveRound();
+
+                transform.DOLocalMove(Vector3.zero, 0.1f).onComplete = () => { isClicking = false; };
+            };
+        }
     }
 }
