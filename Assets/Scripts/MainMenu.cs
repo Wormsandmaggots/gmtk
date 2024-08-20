4b9e328ab5afba020fea52901a7b88182b9e4dfd
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-08-20 14:58:14 +0200

    ,m

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,11 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace DefaultNamespace
 {
-    public class WinScreen : MonoBehaviour
+    public class MainMenu : MonoBehaviour
     {
         public static WinScreen instance;
         
@@ -20,28 +18,21 @@ namespace DefaultNamespace
         public EventSystem es;
 
         private bool clickable = false;
-
-        public static bool win = false;
         
         private void Awake()
         {
             es.firstSelectedGameObject = null;
-            instance = this;
-            win = false;
         }
 
-        public void ShowWinScreen()
+        public void ShowButtons()
         {
             winText.transform.DOMove(winTextTarget.position, 1f);
             button.transform.DOMove(buttonTarget.position, 1f).onComplete = () => { clickable = true; };
             BlurManager.SetBlur(true);
-            win = true;
         }
-
+        
         public void PlayTransition()
         {
-            if (!clickable) return;
-            
             clickable = false;
             Transition.PlayAnim();
 
