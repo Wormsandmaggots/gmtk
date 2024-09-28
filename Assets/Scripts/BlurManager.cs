893563290c80b37cdec65de109bee3793853d7ae
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2024-09-28 20:10:17 +0200

    Fix
    Fix colliding with line
    Fix ugly blur, replace with post process

--- Assets/Scripts/BlurManager.cs
+++ Assets/Scripts/BlurManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using DG.Tweening;
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class BlurManager : MonoBehaviour
 {
+    private static BlurManager instance;
     private static Camera blurCamera;
     public static RenderTexture renderTexture;
     private static Material blurMaterialStatic;
@@ -14,9 +17,16 @@ public class BlurManager : MonoBehaviour
     [SerializeField] private GameObject blurPanel;
     [SerializeField] private float blurValue = 0.01f;
     private static float blurValueStatic;
+    private static DepthOfField depthOfFieldStatic;
     
     void Awake()
     {
+        instance = this;
+        if (FindAnyObjectByType<Volume>().profile.TryGet(out DepthOfField tmp))
+        {
+            depthOfFieldStatic = tmp;
+        }
+        
         blurCamera = Camera.main.transform.GetChild(0).GetComponent<Camera>();
 
         blurPanelStatic = blurPanel;
@@ -27,26 +37,50 @@ public class BlurManager : MonoBehaviour
             blurCamera.targetTexture.Release();
         }
 
-        renderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32, 1);
-        blurCamera.targetTexture = renderTexture;
-        blurMaterialStatic = blurMaterial;
-        blurMaterialStatic.SetTexture("_RenderTexture", renderTexture);
-        blurMaterialStatic.SetFloat("_blur", 0);
+        //renderTexture = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32, 1);
+        //blurCamera.targetTexture = renderTexture;
+        //blurMaterialStatic = blurMaterial;
+        //blurMaterialStatic.SetTexture("_RenderTexture", renderTexture);
+        //blurMaterialStatic.SetFloat("_blur", 0);
 
         blurCamera.gameObject.SetActive(false);
     }
 
     public static void SetBlur(bool value)
     {
-        blurCamera.gameObject.SetActive(value);
-        
+        instance.StartCoroutine(LerpFocusDistance(value));
+    }
+
+    private static IEnumerator LerpFocusDistance(bool value)
+    {
+        float targetValue = value ? 0 : 20;
+        float currentValue = depthOfFieldStatic.focusDistance.value;
+        float speed = 10f;
+
         if (value)
+            currentValue = 5f;
+
+        while (true)
         {
-            blurPanelStatic.SetActive(value);
-            blurMaterialStatic.DOFloat(blurValueStatic, "_blur", 0.2f);
+            float add = 0;
+
+            add = speed * Time.deltaTime;
+
+            if (value)
+                add = -add;
+
+            currentValue += add;
+
+            depthOfFieldStatic.focusDistance.value = currentValue;
+
+            if (value && currentValue < 0)
+                yield break;
+            
+            if(!value && currentValue > 20)
+                yield break;
+
+            yield return null;
         }
-        else
-            blurMaterialStatic.DOFloat(0.0f, "_blur", 0.2f).onComplete = () => blurPanelStatic.SetActive(value);
     }
     
 }
