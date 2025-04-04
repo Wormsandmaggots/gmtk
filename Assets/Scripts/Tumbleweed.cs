f74e0655d9faf062212ffc2813333d0b42bbf59d
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2025-04-04 15:55:44 +0200

    Better tumbleweed

--- Assets/Scripts/Tumbleweed.cs
+++ Assets/Scripts/Tumbleweed.cs
@@ -9,22 +9,28 @@ public class Tumbleweed : MonoBehaviour
     [SerializeField] private float _animTime = 5.0f;
     [SerializeField] private float _jumpFrequency = 1.0f;
     [SerializeField] private float _rotationSpeed = 1.0f;
+    [SerializeField] private float _bumpRotationMul = 2f;
     [SerializeField] private Vector3 _rotationAxis = Vector3.right;
     [SerializeField] private Transform _pointDirection;
+    [SerializeField] private bool _useScaling = false;
     [SerializeField] private VisualEffect _visualEffect;
     private Vector3 _deltaPos;
     private Vector3 _startPos;
     private Vector3 _dir;
     private float _timer;
     private float _epsilon = 0.1f;
+    private Vector3 _startScale;
 
     private void Start()
     {
         _startPos = transform.position;
-        
-        _dir.x = _pointDirection.localPosition.x;
-        _dir.z = _pointDirection.localPosition.z;
+
+        Vector3 d = _pointDirection.position - transform.position;
+        _dir.x = d.x;
+        _dir.z = d.z;
         _dir = _dir.normalized;
+        
+        _startScale = transform.localScale;
     }
 
     private void Update()
@@ -46,8 +52,22 @@ public class Tumbleweed : MonoBehaviour
         if (transform.position.y <= _startPos.y + _epsilon)
         {
             _visualEffect.Stop();
+            
             _visualEffect.transform.position = new Vector3(transform.position.x, _visualEffect.transform.position.y,
                 transform.position.z);
+
+            if (_useScaling)
+            {
+                Vector3 scaleVector = Vector3.forward;
+                scaleVector += Vector3.right;
+                
+                transform.DOScale(transform.localScale + scaleVector * -0.05f, 0.1f).onComplete = () =>
+                {
+                    transform.DOScale(_startScale, 0.1f);
+                };
+                transform.Rotate(_rotationAxis, _rotationSpeed * _bumpRotationMul);
+            }
+            
             _visualEffect.Play();
         }
     }
