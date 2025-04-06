25220bcf59f9c99222cac158a4f465e9d9ca2935
RSobolewski <72765186+Wormsandmaggots@users.noreply.github.com>
2025-04-06 11:46:14 +0200

    added screen space shadows

---
+++ Assets/Scripts/ShadowMapCaptureFeature.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ShadowMapCaptureFeature : ScriptableRendererFeature
+{
+    class ShadowMapPass : ScriptableRenderPass
+    {
+        RenderTargetIdentifier shadowMap = new("_CameraDepthTexture");
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var shadowMap = Shader.GetGlobalTexture("_ScreenSpaceShadowmapTexture");
+            if (shadowMap != null)
+            {
+                Shader.SetGlobalTexture("_ShadowMap", shadowMap);
+            }
+            else
+            {
+                Debug.LogWarning("Main Light Shadow Map is null — URP might be hiding it.");
+            }
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, FrameResources frameResources,
+            ref RenderingData renderingData)
+        {
+            TextureHandle mainShadowsTexture = frameResources.GetTexture(UniversalResource.MainShadowsTexture); 
+
+            if (mainShadowsTexture.IsValid())
+                Shader.SetGlobalTexture("_Shadows", mainShadowsTexture);
+        }
+    }
+
+    ShadowMapPass shadowMapPass;
+
+    public override void Create()
+    {
+        shadowMapPass = new ShadowMapPass
+        {
+            renderPassEvent = RenderPassEvent.AfterRenderingShadows
+        };
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(shadowMapPass);
+    }
+}
